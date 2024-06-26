61fbb46f4ed50d6856dfea8fc9eed329d026a641
paarth <singh.paarth10@gmail.com>
2024-06-26 16:05:31 +0530

    test

---
+++ three_numbers_sum.py
@@ -0,0 +1,9 @@
+print("\t\t*****INPUT*****")
+n1 = int(input("Enter 1st Number:"))
+n2 = int(input("Enter 2nd Number:"))
+n3 = int(input("Enter 3rd Number:"))
+print("\t\t*****OUTPUT*****")
+if (n1 == n2 == n3):
+    print("The Sum is :",(n1+n2+n3)*3)
+else:
+    print("The Sum is :",(n1+n2+n3))
